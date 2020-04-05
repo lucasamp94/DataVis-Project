bdffcac09f0c9912a0dc209efc4bd35974b6f6dd
Gabriele <Gabbosaur>
2020-04-05 19:47:49 +0200

    fixed reading csv, now issue with barchart

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,118 @@
+// Global variable containing all the Dataset
+let nutData;
+
+let fasceEta = [100, 25, 25, 50, 25];
+let idpersona = [];
+
+
+function calculateAgeArray(subject, eta) {
+    let flag = false;
+    for (let i = 0; i < idpersona.length; i++) {
+        if (subject == idpersona[i]) {  // controllo l'esistenza della persona nell'array
+            flag = true;
+        }
+    }
+    if (flag == false) {
+        idpersona.push(subject);
+        if (eta < 30) {
+            fasceEta[0]++; // 20+ perché abbiamo controllato che non esistono persone di meno di 20 anni
+        }
+        else if (eta < 40) {
+            fasceEta[1]++; //30+
+        }
+        else if (eta < 50) {
+            fasceEta[2]++; // 40+
+        }
+        else if (eta < 60) {
+            fasceEta[3]++; // 50+
+        }
+        else {
+            fasceEta[4]++; // 60+
+        }
+    }
+}
+
+
+// D3 code
+function createLeftBarChart() {
+
+    // dimensions and margins of the graph
+    let margin = {
+        top: 5,
+        right: 10,
+        bottom: 5,
+        left: 20
+    }
+
+    let width = 200 - margin.left - margin.right;
+    let height = 200 - margin.top - margin.bottom;
+    let altezzaBar = 30;
+    // ranges
+    let x = d3.scaleLinear()
+        .range([0, width]);
+
+    let y = d3.scaleBand()
+        .range([height, 0])
+        .padding(0.1);
+
+    // append svg object to body of the page
+    let svg = d3.select("#persone").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // scale
+    x.domain([0, d3.max(fasceEta, function (d) {
+        return d;
+    })]);
+    y.domain(fasceEta.map(function (d) {
+        return d;                           // <--- Check qua, guardare esercizi prof per BARCHAR ORIZZONTALE
+    }));
+
+    // append rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(fasceEta)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("width", function (d) {
+            return x(d);
+        })
+        .attr("y", function (d) {
+            return 200 - altezzaBar - y(d); // <--- PROBLEMA QUI o su asse Y in generale
+                                            // creare append per il g TEXT + BAR
+        })
+        .attr("height", altezzaBar);
+
+
+};
+
+
 // Load CSV file
 d3.csv("data/dc.csv", function (error, csv) {
     if (error) {
         console.log(error);  //Log the error.
         throw error;
     }
-
+    console.log('helloworld');
     csv.forEach(function (d) {
 
         // Convert numeric values to 'numbers'
-        d.subject = +d.SUBJECT;
+        d.SUBJECT = +d.subject;
         // date
         // session
-        d.qn_score = +d.QN_SCORE;
-        d.age = +d.AGE;
-        d.familysize = +d.FAMILYSIZE;
+        d.QN_SCORE = +d.QN_score;
+        d.AGE = +d.age;
+        d.FAMILYSIZE = +d.familysize;
         // children
-        d.income = +d.INCOME;
+
         // female
-        d.boughtproduct = +d.BOUGHTPRODUCT;
-        d.code = +d.CODE;
-        d.quantity = +d.QUANTITY; // 6 bottiglie
-        d.nproducts = +d.NPRODUCTS; // 1 cesta di acqua
-        d.nitem = +d.NITEM; // quantity * nproducts = 6*1 = 6 total items
-        d.caddy = +d.CADDY; 
+        d.BOUGHTPRODUCT = +d.boughtproduct;
+        d.CODE = +d.code;
+        d.QUANTITY = +d.quantity; // 6 bottiglie
+        d.NPRODUCTS = +d.Nproducts; // 1 cesta di acqua
+        d.NITEM = +d.Nitem; // quantity * nproducts = 6*1 = 6 total items
+        d.CADDY = +d.caddy;
         // bought
         // disp
         // ingr
@@ -30,54 +120,58 @@ d3.csv("data/dc.csv", function (error, csv) {
         // Ndisp
         // Ningr
         // Nnutr
-        d.price_kg_l = +d.PRICE_KG_L;
-        d.price = +d.PRICE;
-        d.weight = +d.WEIGHT;
+        d.PRICE_KG_L = +d.Price_kg_l;
+        d.PRICE = +d.price;
+        d.WEIGHT = +d.weight;
 
-        d.kcal_100g = +d.KCAL_100G;
-        d.lipides_100g = +d.LIPIDES_100G;
-        d.ags_100g = +d.AGS_100G;
-        d.glucides_100g = +d.GLUCIDES_100G;
-        d.sucres_100g = +d.SUCRES_100G;
-        d.proteines_100g = +d.PROTEINES_100G;
-        d.sel_100g = +d.SEL_100G;
-        d.fibres_100g = +d.FIBRES_100G;
-        d.kcal_portion = +d.KCAL_PORTION;
-        d.kcal_pourcent = +d.KCAL_POURCENT;
-        d.lipides_portion = +d.LIPIDES_PORTION;
-        d.lipides_pourcent = +d.LIPIDES_POURCENT;
-        d.ags_portion = +d.AGS_PORTION;
-        d.ags_pourcent = +d.AGS_POURCENT;
-        d.sucre_portion = +d.SUCRE_PORTION;
-        d.sucre_pourcent = +d.SUCRE_POURCENT;
-        d.sel_portion = +d.SEL_PORTION;
-        d.sel_pourcent = +d.SEL_POURCENT;
-        d.score = +d.SCORE;
+        d.KCAL_100G = +d.Kcal_100g;
+        d.LIPIDES_100G = +d.Lipides_100g;
+        d.AGS_100G = +d.AGS_100g;
+        d.GLUCIDES_100G = +d.Glucides_100g;
+        d.SUCRES_100G = +d.Sucres_100g;
+        d.PROTEINES_100G = +d.Proteines_100g;
+        d.SEL_100G = +d.Sel_100g;
+        d.FIBRES_100G = +d.Fibres_100g;
+        d.KCAL_PORTION = +d.kcal_portion;
+        d.KCAL_POURCENT = +d.Kcal_pourcent;
+        d.LIPIDES_PORTION = +d.Lipides_portion;
+        d.LIPIDES_POURCENT = +d.Lipides_pourcent;
+        d.AGS_PORTION = +d.AGS_portion;
+        d.AGS_POURCENT = +d.AGS_pourcent;
+        d.SUCRE_PORTION = +d.Sucre_portion;
+        d.SUCRE_POURCENT = +d.Sucre_pourcent;
+        d.SEL_PORTION = +d.Sel_portion;
+        d.SEL_POURCENT = +d.Sel_pourcent;
+        d.SCORE = +d.score;
         // lim, non presente in csv
 
         // labels
-        d.nutriscore = [+d.NUTRISCORE_COLOR, +d.NUTRISCORE_LETTER];
-        d.trafficlight = [+d.TL_FAT_COLOR, +d.TL_AGS_COLOR, +d.TL_SUGAR_COLOR, +d.TL_SALT_COLOR];
-        d.nutriscorepartial = [+d.NUTRISCORE_PARTIAL_COLOR, +d.NUTRISCORE_PARTIAL_LETTER];
+        d.NUTRISCORE = [d.NutriScore_color, d.NutriScore_letter];
+        d.TRAFFICLIGHT = [d.TL_fat_color, d.TL_AGS_color, d.TL_sugar_color, d.TL_salt_color];
+        d.NUTRISCOREPARTIAL = [d.NutriScore_partial_color, d.NutriScore_partial_letter];
 
 
         //Break up lists into javascript arrays
-        d.treatment = d3.csvParse(d.TREATMENT).columns;
-        d.profession = d3.csvParse(d.PROFESSION).columns;
-        d.product = d3.csvParse(d.PRODUCT).columns;
-        d.brand = d3.csvParse(d.BRAND).columns;
-        d.unit_prix_kg_l = d3.csvParse(d.UNIT_PRIX_KG_L).columns;
-        d.unit_weight = d3.csvParse(d.UNIT_WEIGHT).columns;
+        d.INCOME = d.income;
+        d.TREATMENT = d.treatment;
+        d.PROFESSION = d.profession;
+        d.PRODUCT = d.product;
+        d.BRAND = d.brand;
+        d.UNIT_PRIX_KG_L = d.Unit_prix_kg_l;
+        d.UNIT_WEIGHT = d.unit_weight;
         // portion
-        
 
 
+        // Function that checks Subject uniqueness
+        //calculateAgeArray(d.SUBJECT, d.AGE);
     });
 
     // Store csv data in a global variable
     nutData = csv;
     // Draw the Bar chart for the first time
-    createBarChart('attendance');
-
+    console.log(fasceEta);
+    console.log(idpersona);
     console.log(nutData);
+    createLeftBarChart();
 });
+
